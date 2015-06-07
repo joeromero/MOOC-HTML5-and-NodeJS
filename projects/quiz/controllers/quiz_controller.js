cfafd6fbaef8144e1b74091f849b080cbb5b8130
Carlos Gómez <cfgc2828@gmail.com>
2015-06-07 09:41:35 +0200

    Get a list of questions

--- projects/quiz/controllers/quiz_controller.js
+++ projects/quiz/controllers/quiz_controller.js
@@ -1,19 +1,49 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
-exports.question = function(req, res) {
-	models.Quiz.findAll().success(function(quiz) {
-		res.render('quizes/question', { pregunta: quiz[0].pregunta });
-	});
+// Autoload
+exports.load = function(req, res, next, quizId) {
+  models.Quiz.findById(quizId).then(
+	function(quiz) {
+      if (quiz) {
+        req.quiz = quiz;
+        next();
+      } else{ next(new Error('No existe quizId=' + quizId)); }
+    }
+  ).catch(function(error) { next(error);} );
 };
 
-// GET /quizes/answer
-exports.answer = function(req, res) {
-	models.Quiz.findAll().success(function(quiz) {
-		if(req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer', { respuesta: 'Correcto' });
-		} else {
-			res.render('quizes/answer', { respuesta: 'Incorrecto' });
+// GET /quizes
+exports.index = function(req, res) {
+	models.Quiz.findAll().then(
+		function(quizes) {
+			res.render('quizes/index', { quizes: quizes });
 		}
-	});
+  	).catch(function(error) { next(error);} );
+};
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+	res.render('quizes/show', { quiz: req.quiz });
+//	models.Quiz.find(req.params.quizId).then(function(quiz) {
+//		res.render('quizes/show', { quiz: quiz });
+//	});
+};
+
+// GET /quizes/:id/answer
+exports.answer = function(req, res) {
+	var resultado = 'Incorrecto';
+	if(req.query.respuesta === req.quiz.respuesta){
+		resultado: 'Correcto';
+	} 
+	res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado });
+	
+//	models.Quiz.find(req.params.quizId).then(function(quiz) {
+//		if(req.query.respuesta === quiz.respuesta){
+//			res.render('quizes/answer', 
+//			{ quiz: quiz, respuesta: 'Correcto' });
+//		} else {
+//			res.render('quizes/answer', 
+//			{ quiz: quiz, respuesta: 'Incorrecto' });
+//		}
+//	});
 };
--- projects/quiz/routes/index.js
+++ projects/quiz/routes/index.js
@@ -8,7 +8,16 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
+//router.get('/quizes/question', quizController.question);
+//router.get('/quizes/answer', quizController.answer);
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
 
 module.exports = router;
